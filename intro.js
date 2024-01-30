5868072e032a07a4277b7e8c24d7d214b31c1881
sxskey <83065600+syskey4603@users.noreply.github.com>
2024-01-30 17:14:43 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,14 @@
+let loginvalue = 24248
+
+let loginval = document.getElementById("loginval")
+let invsubmit = document.getElementById("invsubmit")
+
+document.getElementById('loginval').onkeydown = function(e){
+    if(e.key === 'Enter'){
+        console.log(loginval.value)
+    }
+ };
+
+document.getElementById("invsubmit").onclick = function () {
+    
+};
