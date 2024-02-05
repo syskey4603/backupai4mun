35253a267787276b0574d0f84e3be53e91a66364
sxskey <83065600+syskey4603@users.noreply.github.com>
2024-02-05 21:52:59 +0530

    Update intro.js

--- intro.js
+++ intro.js
@@ -4,14 +4,19 @@ let loginval = document.getElementById("loginval")
 
 
 document.getElementById("invsubmit").onclick = function () {
-    window.alert("a")
     console.log("test")
     if(loginval.value == "maker") {
         window.open("geminihtml.html")
 
     }
-    if(loginval.value == "creator") {
+    if(loginval.value == "answerer") {
         window.open("poianswer.html")
+    }
+    if(loginval.value == "researcher") {
+        window.open("researcher.html")
+    }
+    if(loginval.value == "solution") {
+        window.open("solutions.html")
     }
      
 }
