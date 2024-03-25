284cd77a1e9268f1a42f57b685b0a1e3e22210d6
sxskey <syskeyownsyou@gmail.com>
2024-03-25 09:23:00 +0530

    fixed

--- poimaker/script.js
+++ poimaker/script.js
@@ -95,6 +95,27 @@ async function test() {
   }
   catch(ex)
   {
+    window.alert(ex)
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      }
+    ];
+  
+    const model = genAI.getGenerativeModel({ model: "gemini-pro", safetySettings });
     const temp = await model.generateContent("return an empty string")
     const tempresp = await temp.response;
     const temptext = tempresp.text();
